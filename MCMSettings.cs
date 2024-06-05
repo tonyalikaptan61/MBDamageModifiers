d05cc04ead6a967ccb9ba96d1ddd84c164517542
tonyalikaptan61 <tonyalikaptan61@gmail.com>
2024-06-05 09:43:42 +0300

    Localization supported, same code
    Added localization support.

--- MCMSettings.cs
+++ MCMSettings.cs
@@ -18,15 +18,15 @@ namespace DamageModifiers
 		public override string FormatType => "json";
 
 		#region GENERAL
-		const string GroupNameGeneral = "General";
+		const string GroupNameGeneral = "{=8byRf9zO}General";
 
 		[SettingPropertyFloatingInteger(
-			"Melee Damage",
+			"{=DMG_MO_001}Melee Damage",
 			0f,
 			10f,
 			"0.00",
 			RequireRestart = false,
-			HintText = "Melee damage modifier, multiplicative with other modifiers. [Native: 1.00]",
+			HintText = "{=DMG_MO_001H}Melee damage modifier, multiplicative with other modifiers. [Native: 1.00]",
 			Order = 0)]
 		[SettingPropertyGroup(
 			GroupNameGeneral,
@@ -34,12 +34,12 @@ namespace DamageModifiers
 		public float Melee { get; set; } = 1f;
 
 		[SettingPropertyFloatingInteger(
-			"Ranged Damage",
+            "{=DMG_MO_002}Ranged Damage",
 			0f,
 			10f,
 			"0.00",
 			RequireRestart = false,
-			HintText = "Ranged damage modifier, multiplicative with other modifiers. [Native: 1.00]",
+			HintText = "{=DMG_MO_002H}Ranged damage modifier, multiplicative with other modifiers. [Native: 1.00]",
 			Order = 1)]
 		[SettingPropertyGroup(
 			GroupNameGeneral,
@@ -47,12 +47,12 @@ namespace DamageModifiers
 		public float Ranged { get; set; } = 1f;
 
 		[SettingPropertyFloatingInteger(
-			"Horse Charge Damage",
+            "{=DMG_MO_003}Horse Charge Damage",
 			0f,
 			10f,
 			"0.00",
 			RequireRestart = false,
-			HintText = "Horse charge damage modifier, multiplicative with other modifiers. [Native: 1.00]",
+			HintText = "{=DMG_MO_003H}Horse charge damage modifier, multiplicative with other modifiers. [Native: 1.00]",
 			Order = 2)]
 		[SettingPropertyGroup(
 			GroupNameGeneral,
@@ -61,12 +61,12 @@ namespace DamageModifiers
 
 
 		[SettingPropertyFloatingInteger(
-			"Damage to Shields",
+            "{=DMG_MO_004}Damage to Shields",
 			0f,
 			10f,
 			"0.00",
 			RequireRestart = false,
-			HintText = "Damage modifier against shields, multiplicative with other modifiers. [Native: 1.00]",
+			HintText = "{=DMG_MO_004H}Damage modifier against shields, multiplicative with other modifiers. [Native: 1.00]",
 			Order = 3)]
 		[SettingPropertyGroup(
 			GroupNameGeneral,
@@ -74,12 +74,12 @@ namespace DamageModifiers
 		public float Shields { get; set; } = 1f;
 
 		[SettingPropertyFloatingInteger(
-			"Damage to Objects",
+            "{=DMG_MO_005}Damage to Objects",
 			0f,
 			10f,
 			"0.00",
 			RequireRestart = false,
-			HintText = "Damage modifier against objects, multiplicative with other modifiers. [Native: 1.00]",
+			HintText = "{=DMG_MO_005H}Damage modifier against objects, multiplicative with other modifiers. [Native: 1.00]",
 			Order = 4)]
 		[SettingPropertyGroup(
 			GroupNameGeneral,
@@ -88,12 +88,12 @@ namespace DamageModifiers
 
 
 		[SettingPropertyFloatingInteger(
-			"Fall Damage",
+            "{=DMG_MO_006}Fall Damage",
 			0f,
 			10f,
 			"0.00",
 			RequireRestart = false,
-			HintText = "Damage modifier for fall damage, equal for everyone. [Native: 1.00]",
+			HintText = "{=DMG_MO_006H}Damage modifier for fall damage, equal for everyone. [Native: 1.00]",
 			Order = 5)]
 		[SettingPropertyGroup(
 			GroupNameGeneral,
@@ -102,11 +102,11 @@ namespace DamageModifiers
 		#endregion
 
 		#region BATTLE MODIFIERS
-		const string GroupNameBattle = "Battles etc.";
-		const string HintTextBattle = "Attacker vs Victim in non-arena/tournament fights. Will be further modified by difficulty setting. [Native: 1.00]";
+		const string GroupNameBattle = "{=DMG_MO_007}Battles etc.";
+		const string HintTextBattle = "{=DMG_MO_007H}Attacker vs Victim in non-arena/tournament fights. Will be further modified by difficulty setting. [Native: 1.00]";
 
 		[SettingPropertyFloatingInteger(
-			"Player vs Player",
+            "{=DMG_MO_009}Player vs Player",
 			0f,
 			10f,
 			"0.00",
@@ -119,7 +119,7 @@ namespace DamageModifiers
 		public float BattlePlayerPlayer { get; set; } = 1f;
 
 		[SettingPropertyFloatingInteger(
-			"Hero vs Player",
+            "{=DMG_MO_010}Hero vs Player",
 			0f,
 			10f,
 			"0.00",
@@ -132,7 +132,7 @@ namespace DamageModifiers
 		public float BattleHeroPlayer { get; set; } = 1f;
 
 		[SettingPropertyFloatingInteger(
-			"Other vs Player",
+            "{=DMG_MO_011}Other vs Player",
 			0f,
 			10f,
 			"0.00",
@@ -146,7 +146,7 @@ namespace DamageModifiers
 
 
 		[SettingPropertyFloatingInteger(
-			"Player vs Hero",
+            "{=DMG_MO_012}Player vs Hero",
 			0f,
 			10f,
 			"0.00",
@@ -159,7 +159,7 @@ namespace DamageModifiers
 		public float BattlePlayerHero { get; set; } = 1f;
 
 		[SettingPropertyFloatingInteger(
-			"Hero vs Hero",
+            "{=DMG_MO_013}Hero vs Hero",
 			0f,
 			10f,
 			"0.00",
@@ -172,7 +172,7 @@ namespace DamageModifiers
 		public float BattleHeroHero { get; set; } = 1f;
 
 		[SettingPropertyFloatingInteger(
-			"Other vs Hero",
+            "{=DMG_MO_014}Other vs Hero",
 			0f,
 			10f,
 			"0.00",
@@ -186,7 +186,7 @@ namespace DamageModifiers
 
 
 		[SettingPropertyFloatingInteger(
-			"Player vs Other",
+            "{=DMG_MO_015}Player vs Other",
 			0f,
 			10f,
 			"0.00",
@@ -199,7 +199,7 @@ namespace DamageModifiers
 		public float BattlePlayerOther { get; set; } = 1f;
 
 		[SettingPropertyFloatingInteger(
-			"Hero vs Other",
+            "{=DMG_MO_016}Hero vs Other",
 			0f,
 			10f,
 			"0.00",
@@ -212,7 +212,7 @@ namespace DamageModifiers
 		public float BattleHeroOther { get; set; } = 1f;
 
 		[SettingPropertyFloatingInteger(
-			"Other vs Other",
+            "{=DMG_MO_017}Other vs Other",
 			0f,
 			10f,
 			"0.00",
@@ -226,11 +226,11 @@ namespace DamageModifiers
 		#endregion
 
 		#region ARENA MODIFIERS
-		const string GroupNameArena = "Arena & Tournament";
-		const string HintTextArena = "Attacker vs Victim, in Arena and tournament fights. Will be further modified by difficulty setting. [Native: 1.00]";
+		const string GroupNameArena = "{=DMG_MO_008}Arena & Tournament";
+		const string HintTextArena = "{=DMG_MO_008H}Attacker vs Victim, in Arena and tournament fights. Will be further modified by difficulty setting. [Native: 1.00]";
 
 		[SettingPropertyFloatingInteger(
-			"Player vs Player",
+            "{=DMG_MO_009}Player vs Player",
 			0f,
 			10f,
 			"0.00",
@@ -243,7 +243,7 @@ namespace DamageModifiers
 		public float ArenaPlayerPlayer { get; set; } = 1f;
 
 		[SettingPropertyFloatingInteger(
-			"Hero vs Player",
+            "{=DMG_MO_0010}Hero vs Player",
 			0f,
 			10f,
 			"0.00",
@@ -256,7 +256,7 @@ namespace DamageModifiers
 		public float ArenaHeroPlayer { get; set; } = 1f;
 
 		[SettingPropertyFloatingInteger(
-			"Other vs Player",
+            "{=DMG_MO_011}Other vs Player",
 			0f,
 			10f,
 			"0.00",
@@ -270,7 +270,7 @@ namespace DamageModifiers
 
 
 		[SettingPropertyFloatingInteger(
-			"Player vs Hero",
+            "{=DMG_MO_012}Player vs Hero",
 			0f,
 			10f,
 			"0.00",
@@ -283,7 +283,7 @@ namespace DamageModifiers
 		public float ArenaPlayerHero { get; set; } = 1f;
 
 		[SettingPropertyFloatingInteger(
-			"Hero vs Hero",
+            "{=DMG_MO_013}Hero vs Hero",
 			0f,
 			10f,
 			"0.00",
@@ -296,7 +296,7 @@ namespace DamageModifiers
 		public float ArenaHeroHero { get; set; } = 1f;
 
 		[SettingPropertyFloatingInteger(
-			"Other vs Hero",
+            "{=DMG_MO_014}Other vs Hero",
 			0f,
 			10f,
 			"0.00",
@@ -310,7 +310,7 @@ namespace DamageModifiers
 
 
 		[SettingPropertyFloatingInteger(
-			"Player vs Other",
+            "{=DMG_MO_015}Player vs Other",
 			0f,
 			10f,
 			"0.00",
@@ -323,7 +323,7 @@ namespace DamageModifiers
 		public float ArenaPlayerOther { get; set; } = 1f;
 
 		[SettingPropertyFloatingInteger(
-			"Hero vs Other",
+            "{=DMG_MO_016}Hero vs Other",
 			0f,
 			10f,
 			"0.00",
@@ -336,7 +336,7 @@ namespace DamageModifiers
 		public float ArenaHeroOther { get; set; } = 1f;
 
 		[SettingPropertyFloatingInteger(
-			"Other vs Other",
+            "{=DMG_MO_017}Other vs Other",
 			0f,
 			10f,
 			"0.00",
